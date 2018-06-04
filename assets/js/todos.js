3ca8f53d781c5bdc39db5b502d0499dfbdaab151
Mashinigami <damuron@gmail.com>
2018-06-04 02:11:08 -0300

    New input text, an unordered list, some event listeners to grey out and cross a done task, and a X to delete the task.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,18 @@
-console.log("CONNECTED");
+$("ul").on("click", "li", function (e){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(1000,function () {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (e) {
+    if(e.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $('ul').append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
